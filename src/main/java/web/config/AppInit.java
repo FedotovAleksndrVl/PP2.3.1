c161f473b4647c4b7518022ef904809829a0a094
FedotovAleksndrVl <FedotovAleksndrVl@gmail.com>
2024-10-05 21:02:24 +0300

    Completed, fix 3

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -1,11 +1,6 @@
 package web.config;
 
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     // Метод, указывающий на класс конфигурации
